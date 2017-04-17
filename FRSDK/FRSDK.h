7a3dfc227afddb0e2a5fff964480d726a7019e04
HaifengMay <haifeng.mhf@alibaba-inc.com>
2017-04-17 20:44:31 +0800

    Initial Commit

---
+++ FRSDK/FRSDK.h
@@ -0,0 +1,13 @@
+//
+//  FRSDK.h
+//  FRSDK
+//
+//  Created by HaifengMay on 17/04/2017.
+//  Copyright © 2017 HaifengMay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRSDK : NSObject
+
+@end
---
+++ FRSDK/FRSDK.m
@@ -0,0 +1,13 @@
+//
+//  FRSDK.m
+//  FRSDK
+//
+//  Created by HaifengMay on 17/04/2017.
+//  Copyright © 2017 HaifengMay. All rights reserved.
+//
+
+#import "FRSDK.h"
+
+@implementation FRSDK
+
+@end
